26b771bfd299cb79ce5894c7677ccb7f119566bb
satyanandkumbhar <kumbhar.satyanand@gmail.com>
2021-12-01 15:59:13 +0000

    Reading in console input

---
+++ input04.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+    fmt.Print("Enter text: ")
+    reader := bufio.NewReader(os.Stdin)
+    
+    // ReadString will block until the delimiter is entered
+    input, err := reader.ReadString('\n')   // this returns (string, error) and we capture both
+    
+    // simple error handling
+    if err != nil {
+        fmt.Println("An error occurred while reading input. Please try again", err)
+        return
+    }
+
+    // remove the delimiter from the string
+    input = strings.TrimSuffix(input, "\n")   // A "Suffix" means added to "the end of"
+    fmt.Println(input)   // "fmt.Println" has a built in "\n" it will append to any string
+}
+
